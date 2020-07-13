c3b8c62bc34f9213c7fd5158eb26aabc0be7836d
vivek <mehra.viv@gmail.com>
2020-07-13 18:58:58 +0530

    Added conditional rendering for output screen

--- src/components/outputComponents/OutputBodyComponent.tsx
+++ src/components/outputComponents/OutputBodyComponent.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
-
-class OutputBodyComponent extends React.Component {
+import { connect } from "react-redux";
+type Props = {
+  isModelTrained: boolean;
+};
+class OutputBodyComponent extends React.Component<Props> {
   render() {
+    const untrainedModel = (
+      <p>You must train a model on the left before you can preview it here.</p>
+    );
+    const trainedModel = <h3>Output Screen will come</h3>;
     return (
-      <div>
-        <p>
-          You must train a model on the left before you can preview it here.
-        </p>
-      </div>
+      <div>{this.props.isModelTrained ? trainedModel : untrainedModel}</div>
     );
   }
 }
-export default OutputBodyComponent;
+const mapStateToProps = (state) => {
+  return {
+    isModelTrained: state.isModelTrained,
+  };
+};
+export default connect(mapStateToProps)(OutputBodyComponent);
